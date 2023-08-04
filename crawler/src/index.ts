8b00af3fd8dcdeba06ecc77435c9e5d3524aefed
wintree86 <mith1004@gmail.com>
2023-08-04 17:05:27 +0900

    Ch02 - 02 puppeteer 기본 사용법

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -8,10 +8,32 @@ const initBrowser = async () => {
   return browser;
 };
 
+const login = async (page: puppeteer.Page) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.type("#user-name", "standard_user");
+  await page.type("#password", "secret_sauce");
+  await page.click("#login-button");
+};
+
+const clickPage = async (page: puppeteer.Page) => {
+  await page.click("#item_4_img_link");
+};
+
+const collectProductInfos = async (page: puppeteer.Page) => {
+  return page.evaluate(() => {
+    let productNames: string[] = [];
+    const elements = document.querySelectorAll(".inventory_item_name");
+    elements.forEach((element) => productNames.push(element.innerHTML));
+    return productNames;
+  });
+};
+
 (async () => {
   const browser = await initBrowser();
   const page = await browser.newPage();
-  
-  // NEXT
+  await login(page);
+  //   await clickPage(page);
+  const infos = await collectProductInfos(page);
+  console.log(infos);
   await browser.close();
 })();
